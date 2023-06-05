959eb32f2b20d224077687b4dd3a081145b9fdce
Marley <marleyrae1997@gmail.com>
2023-06-05 16:23:25 -0700

    :tada: chore: HTML & interactivity.

--- interactive-rating-component/src/main.ts
+++ interactive-rating-component/src/main.ts
@@ -2,5 +2,10 @@ import { createApp } from 'vue'
 import './style.css'
 
 createApp({
-    
+    data() {
+        return {
+            rating: 0,
+            submitted: false,
+        }
+    }
 }).mount('#app')
